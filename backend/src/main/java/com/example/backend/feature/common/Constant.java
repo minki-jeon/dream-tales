425dde5e6fd39d7ca7ff32ade7ece91a424c06b3
minki-jeon <minki.jeon.study@gmail.com>
2025-09-04 17:12:26 +0900

    상수 관리 클래스

---
+++ backend/src/main/java/com/example/backend/feature/common/Constant.java
@@ -0,0 +1,17 @@
+package com.example.backend.feature.common;
+
+/**
+ * <pre>
+ * packageName    : com.example.backend.feature.common
+ * fileName       : Constant
+ * author         : minki-jeon
+ * date           : 2025-09-04 (목)
+ * description    : 상수 관리
+ * ===========================================================
+ * DATE                     AUTHOR           NOTE
+ * -----------------------------------------------------------
+ * 2025-09-04 (목)        minki-jeon       최초 생성
+ * </pre>
+ */
+public class Constant {
+}
