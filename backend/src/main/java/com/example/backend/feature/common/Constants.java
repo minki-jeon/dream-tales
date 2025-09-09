3481fcb159fddb547ed7655cdec52cdc394b001c
minki-jeon <minki.jeon.study@gmail.com>
2025-09-09 11:17:33 +0900

    이미지 생성을 위한 최적화 프롬프트 추가, 이미지 모델 추가

--- backend/src/main/java/com/example/backend/feature/common/Constants.java
+++ backend/src/main/java/com/example/backend/feature/common/Constants.java
@@ -13,6 +13,7 @@ package com.example.backend.feature.common;
  * 2025-09-04 (목)        minki-jeon       최초 생성
  * 2025-09-05 (금)        minki-jeon       Rename Constant to Constants
  * 2025-09-05 (금)        minki-jeon       Add ApiUrl, Model
+ * 2025-09-09 (화)        minki-jeon       Add Optimization Image Prompt, Add Image Model
  * </pre>
  */
 public final class Constants {
@@ -35,9 +36,43 @@ public final class Constants {
      */
     public static final String MODEL_OPENAI_DALL_E_2 = "dall-e-2";
     public static final String MODEL_OPENAI_GPT_4O_MINI = "gpt-4o-mini-2024-07-18";
+    public static final String MODEL_OPENAI_GPT_IMAGE_1 = "GPT-image-1";
 
     /**
      * PROMPT
      */
+    public static final String PROMPT_TRANSLATE_INPUT = """
+            내가 제공한 문장을 이미지를 얻기 위해 dall-e-2가 이해하기 쉽도록 구체적이고 적절한 프롬프트로 수정해주세요. 
+            동화 그림의 이미지가 출력되도록 프롬프트에 내용을 추가해주세요. 
+            dall-e-2가 이해하기 쉽도록 프롬프트를 영어로 번역해서 제공해주세요. 
+            제공할 프롬프트 외에는 응답하지마세요.
+            """;
+    public static final String PROMPT_CREATE_IMAGE_HEADER_SHORT = "Storybook illustration of: \"";
+    public static final String PROMPT_CREATE_IMAGE_HEADER_DETAIL = "Create a storybook-style illustration base on the following description: \"";
+    public static final String PROMPT_CREATE_IMAGE_OPTIMIZATION_SHORT = """
+            \"
+            Style: watercolor, pastel colors, child-friendly, soft lines\n
+            Mood: warm, whimsical, imaginative\n
+            High quality, full color\n
+            """;
+    public static final String PROMPT_CREATE_IMAGE_OPTIMIZATION_MIDDLE = """
+            \"
+            Requirements: \n
+              - Style: watercolor, pastel colors, child-friendly, soft lines, hand-drawn illustration\n
+              - Atmosphere: warm, whimsical, and imaginative, suitable for a children's picture book\n
+              - Resolution: high quality, detailed, full color\n
+            """;
+    public static final String PROMPT_CREATE_IMAGE_OPTIMIZATION_DETAIL = """
+            \"
+            Requirements: \n
+              - Style: watercolor, pastel colors, child-friendly, soft lines, hand-drawn illustration\n
+              - Atmosphere: warm, whimsical, and imaginative, suitable for a children's picture book\n
+              - Characters: consistent and expressive, with friendly and gentle facial expressions\n
+              - Background: simple but colorful, enhancing the storytelling without overwhelming\n
+              - Resolution: high quality, detailed, full color\n
+                \n
+              Output format: single illustration\n
+            """;
+
 
 }
